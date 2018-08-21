238d6845015b493d3d0a8eeccca2d89935c81f63
Sagoza <ergaster.buissness@hotmail.com>
2018-08-21 19:30:26 +0100

    DailyScheduleGen Update 0.1
    New Stuff

--- DailyScheduleGen.java
+++ DailyScheduleGen.java
@@ -4,40 +4,85 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;  
 import java.util.Calendar;  
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
  // És o rei ja derreti o cerebro
 public class DailyScheduleGen {
 	static String dbc = "jdbc:mysql://localhost:3306/transactions";
 	static String dbc_user = "root";
 	static String dbc_password = "";
-	static DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	
 
 	static Scanner scanner = new Scanner(System.in);
 	static String CreateUserQuery = "CREATE TABLE %s (Date DATE, DayType SMALLINT, EnterHour TIME, ExitHour TIME, LunchBreakStart TIME, LunchBreakEnd TIME);";
-
+	//INSERT INTO schedule2018 (Date, DayType, EnterHour, ExitHour, LunchBreakStart, LunchBreakEnd) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');
+	static String DayInsertQuery = "INSERT INTO %s VALUES ('%s', '%s', '%s', '%s', '%s', '%s');";
+	
+	static String Workday = "INSERT INTO %s  VALUES ('%s', '0', '09:00:00', '17:00:00', '12:00:00', '13:00:00');";
+	static String Holyday = "INSERT INTO %s  VALUES ('%s', '1', '00:00:00', '00:00:00', '00:00:00', '00:00:00');";
+	static String ExtraWorkday = "INSERT INTO %s  VALUES ('%s', '2', '%s', '%s', '00:00:00', '00:00:00');";
+	
+	//////////////////////////////////////////
+	// Day Types /////////////////////////////
+	//////////////////////////////////////////
+	// Workday: 0 ////////////////////////////
+	// Holyday: 1 ////////////////////////////
+	// Extra-Work: 2 /////////////////////////
+	//////////////////////////////////////////
+	
+	static Connection myConn;
+	static Statement myStat;
+	
 	public static void main(String[] args) {
 		//String[] Name = userInput("Enter new user First and Last name: ").split(" ");
 		//String ScheduleName = userInput("Enter schedule name: ");
 		Calendar tc; 
-		//MonthLastDay(4);
 		
 		String days = userInput(String.format("Please insert %s(%s) Holydays:", MonthName(0), 0));
 		String[] holydays = days.split(",");
 		
+		int Month =  0;
+		String DB = "schedule2018";
+		
 		try {
 			int[] holydaysInt = ConvertStringList(holydays);
 			
-			for (int i = 1; i <= MonthLastDay(0); i++)
+			for (int i = 1; i <= MonthLastDay(Month); i++)
 			{
+				String MonthDateLocal = ReturnDate(Month, i);
+				
 				for (int hd : holydaysInt)
 				{
 					if (i == hd)
 					{
-						println(String.format("Day %s is a 		Holyday", i));
+						try {
+							myConn = DriverManager.getConnection(dbc, dbc_user, dbc_password);
+							myStat = myConn.createStatement();
+							
+							if (myStat.executeUpdate(String.format(Holyday, DB, MonthDateLocal)) == 1) {
+								println(String.format("Day %s is a Holyday", MonthDateLocal));
+							}
+
+						} catch (Exception exc) {
+							exc.printStackTrace();
+						}
+						
 						i++;
 					}
 				}
-				println(String.format("Day %s is a Workday", i));
+				try {
+					myConn = DriverManager.getConnection(dbc, dbc_user, dbc_password);
+					myStat = myConn.createStatement();
+					
+					if (myStat.executeUpdate(String.format(Workday, DB, MonthDateLocal)) == 1) {
+						println(String.format("Day %s is a Workday", MonthDateLocal));
+					}
+
+				} catch (Exception exc) {
+					exc.printStackTrace();
+				}
+				
+				//println(String.format("Day %s is a Workday", i));
 			}
 		}
 		catch (java.lang.NumberFormatException exc)
@@ -67,6 +112,16 @@ public class DailyScheduleGen {
 		//println(String.format("Month %s(%s) maxDay(%s)", c.getTime(), Month, c.getActualMaximum(Calendar.DATE)));
 	    return c.getActualMaximum(Calendar.DATE);
 	}
+	public static String ReturnDate(int Month, int day) {
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.MONTH, Month);
+		c.set(Calendar.DATE, day);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String Time = sdf.format(c.getTime());
+	    return Time;
+	} 
+	
 	static String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	public static String MonthName (int month)
 	{
