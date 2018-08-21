24367db0785e5fa37fd90f813272419daf73d7df
Renato Fins <pudim.fins@gmail.com>
2018-08-21 10:32:52 +0100

    push.01

--- DailyScheduleGen.java
+++ DailyScheduleGen.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;  
 import java.util.Date;
 import java.util.Scanner;
-
+ // És o rei ja derreti o cerebro
 public class DailyScheduleGen {
 	static String dbc = "jdbc:mysql://localhost:3306/transactions";
 	static String dbc_user = "root";
