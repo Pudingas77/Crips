79ff2352cf036e18ec1daae72400a87d9c82564f
Renato Fins <pudim.fins@gmail.com>
2018-08-29 13:11:42 +0100

    ver

---
+++ Offices.java
@@ -0,0 +1,41 @@
+package Crips;
+
+import java.util.Date;
+
+public class Offices {
+    private static final boolean NoOverlapThursday = false;
+    private static final boolean NoOverlapWednesday = false;
+    
+    public Offices(int OfficeID, int NumberTill) {
+	int type = FunctionsSQL.OfficeCount(OfficeID);
+	if (type == 1) {
+	  //Only one worker
+	    if (NumberTill == 1) {
+	
+	    }
+	    else{
+		System.out.println("You can not have more then 1 Till in a office that only has one employee");
+	    }
+	} else if (type == 2) {
+	    // 2 workers
+	    if(NumberTill == 1){
+		System.out.println("Please choose what type of schedule you want to implement.");
+		System.out.println("\n"+"No Overlap with ThursdayOff or No Overlap with WednesdayOff");
+		if(NoOverlapThursday == true){
+		    System.out.println("You choose the No Overlap Thursday" + "\n" + "Please insert number of entering hour for first employee");
+		    if(EnteringHour != OfficeOpen){
+			System.out.println("Are you sure you want to open at: " + EnteringHour + " Office should open at: " + OfficeOpen);
+		    }
+		}
+		if(NoOverlapWednesday == true){
+		    
+		}
+	    }
+	   
+
+	} else if (type == 3) {
+	    // 3 workers
+	}
+
+    }
+}
